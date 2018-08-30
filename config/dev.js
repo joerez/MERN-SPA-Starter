af21a53933d9af5ba935fa7fad2626fd9a06caa1
Joe Rezendes <rezenbiz@gmail.com>
2018-08-30 16:38:58 -0700

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 //prod.js - production keys
 module.exports = {
-  googleClientID: '59073675446-lirs6dc3im43l2umtc0q80t6q2njmak5.apps.googleusercontent.com',
-  googleClientSecret: '8dHzFOXc4zBURtawZRZG2GTs',
+  googleClientID: 'PUT YOURS HERE',
+  googleClientSecret: 'PUT YOURS HERE',
   cookieKey: 'cookie',
 }

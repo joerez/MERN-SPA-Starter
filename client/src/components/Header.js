04335977970ee563a0680d2e593b7c1267ec530b
Joe Rezendes <rezenbiz@gmail.com>
2018-08-30 17:56:38 -0700

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div className="nav">Campaign Manager</div>
+      <div className="nav">Header</div>
     )
   }
 }
